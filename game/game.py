dbe5681971fa3f770e2b08c3e2547c22ac718e2a
Henry <emeraldwolf356@gmail.com>
2026-09-02 12:28:52 +1000

    erase > clear

--- game/game.py
+++ game/game.py
@@ -2,13 +2,13 @@ import curses
 
 
 def wdwPrint(window, msg, y=0, x=0):
-    window.clear()
+    window.erase()
     window.addstr(y, x, msg)
     window.refresh()
 
 # shows the updated attributes after each fight
 def calcAttrs(player, window):
-    window.clear()
+    window.erase()
     window.addstr(0, 0, "Player: " + player.getName())
     window.addstr(1, 0, "HP: " + str(player.getHP()))
     window.addstr(2, 0, "Gold: " + str(player.getGold()))
